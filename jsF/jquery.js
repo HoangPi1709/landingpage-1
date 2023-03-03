e204e960bf40b541b9afc7cda6f7b6615e959de6
Hoang <phih5066@gmail.com>
2023-03-03 11:28:07 +0700

    First project

---
+++ jsF/jquery.js
@@ -0,0 +1,17 @@
+$(function(){
+    $("#index_nav").load("/_includes/nav.html");
+    $("#index_footer").load("/_includes/footer.html")
+});
+
+function home(){
+    $("#main").load("home.html");
+}
+function page1(){
+    $("#main").load("page1.html");
+}
+function page2(){
+    $("#main").load("page2.html");
+}
+function page3(){
+    $("#main").load("page3.html");
+}
