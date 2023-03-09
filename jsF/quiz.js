ad1e1aa4374159fac8ae8cbb0cd0b2d7a21b7d7c
HoangPi1709 <hoangnpph31117@fpt.edu.vn>
2023-03-09 17:10:28 +0700

    de

---
+++ jsF/quiz.js
@@ -0,0 +1,18 @@
+const quizQuestions = [
+    {
+        question : "HTML là ? ",
+        a: "1",
+        b: "2",
+        c: "3",
+        d: "4",
+        correct: 'c'
+    },
+    {
+        question : "CSS là ? ",
+        a: "1",
+        b: "2",
+        c: "3",
+        d: "4",
+        correct: 'c'
+    }
+]
