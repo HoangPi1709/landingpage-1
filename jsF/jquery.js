7fdecbcf986c5d7ab1977a393f30eddcec9d2b9f
HoangPi1709 <hoangnpph31117@fpt.edu.vn>
2023-03-09 22:43:30 +0700

    fix some file
    fix some file

--- jsF/jquery.js
+++ jsF/jquery.js
@@ -1,12 +1,9 @@
 $(function(){
     $("#index_nav").load("/_includes/nav.html");
-    $("#index_footer").load("/_includes/footer.html")
+    $("#index_footer").load("/_includes/footer.html");
+    $("#main").load("home.html");
 });
 
-function home(){
-    $("#main").load("home.html");
-}
-function page1(){
-    $("#main").load("page1.html");
-}
+
+
 
