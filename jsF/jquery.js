20729dd310b8088f7510fd7a7b384700fb30d451
HoangPi1709 <hoangnpph31117@fpt.edu.vn>
2023-03-12 21:46:53 +0700

    j
    k

--- jsF/jquery.js
+++ jsF/jquery.js
@@ -4,6 +4,21 @@ $(function(){
     $("#main").load("home.html");
 });
 
-
-
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  
+  window.onclick = function(event) {
+    if (!event.target.matches('.nav-icon')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
 
